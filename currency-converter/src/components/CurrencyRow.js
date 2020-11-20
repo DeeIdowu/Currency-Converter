16b4c33c6bb84dee7e9360945706395783bc34ed
Dennis Idowu <dennis_idowu@live.com>
2020-11-20 03:15:43 -0500

    properly imported currency row component into app.js

--- currency-converter/src/components/CurrencyRow.js
+++ currency-converter/src/components/CurrencyRow.js
@@ -3,9 +3,9 @@ import React from 'react'
  function CurrencyRow() {
     return (
         <div>
-            
+            <h1>Currency Row</h1>
         </div>
     )
 }
 
-export default CurrencyRow
+export default CurrencyRow;
