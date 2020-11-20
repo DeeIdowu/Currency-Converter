fa93ff3c2aaead68a637fab585fe3da9dfab97a5
Dennis Idowu <dennis_idowu@live.com>
2020-11-20 03:28:52 -0500

    added api link as base url and utilized useEffect to obtain json response to append the state to currency row and return prop

--- currency-converter/src/App.js
+++ currency-converter/src/App.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import CurrencyRow from './components/CurrencyRow';
 import './App.css';
 
+const BASE_URL="https://api.exchangeratesapi.io/latest"
+
 function App() {
+
+  useEffect(()=>{
+      fetch(BASE_URL)
+        .then(res => res.json())
+        .then(data => console.log(data))
+  }, [])
+
   return (
     <div className="App">
         <h1>Welcome to the Currency Converter</h1>
